f15aaac9f82eb7bda215630264ca6d9dc6fffa8b
Jitendrakumarsaw <jitendrakumarsaw123@gmail.com>
2022-10-31 18:00:21 +0530

    Update Railways.jsx

--- src/pages/Railways.jsx
+++ src/pages/Railways.jsx
@@ -8,7 +8,7 @@ const Railways = () => {
 {/* <div className='text-center bg-pink-100 p-2'>Railways</div> */}
 <div className='bg-black h-screen w-screen flex justify-center items-center uppercase text-white h1 mt-1/2 flex-col'>
       <div className=''>Railways page</div>
-      <button className='bg-white text-slate-500 uppercase p-3 hover:text-blue-500 rounded-sm'><Link to="/">Back</Link></button>
+      <button className='bg-white text-slate-500 uppercase p-3 hover:text-blue-500 rounded-md shadow-inner shadow-slate-300'><Link to="/">Back</Link></button>
 
     </div>
   </>
